e3b9ef4477979519e8a7dbd5a8fa7df2610d5173
Allison McJannet <allison.mcjannet@umontana.edu>
2025-01-19 14:47:41 -0700

    changes

---
+++ HW2/script.js
@@ -0,0 +1,54 @@
+/*
+Assignment Requirements: 
+
+The console.log should include;
+3 of your favorite websites
+3 of your favorite games if you play any - they can be board games, etc.
+3 of your favorite artists and their cultural importance
+Create a simple question and answer that uses prompt and the console.log. 
+You do not need to verify if the answer is correct.
+*/
+
+/* My Favorite Websites*/
+console.log('My Favorite Websites:');
+
+console.log('1.) The Graphic Design Portfolio website I created with the help of Adobe Portfolio.')
+console.log ('- https://allisonmcjannet.myportfolio.com/')
+
+console.log ('2.) Canva. I have created a whole 40 page Yearbook, along with many other projects on this website. It does not beat the Adobe Suite but is wonderful for quick and easy projects.')
+console.log ('- https://www.canva.com/')
+
+console.log ('3.) dafont.com has been a resource I have used for a long time for projects since learning design on the computer in middle school.')
+console.log ('- https://www.dafont.com/')
+
+/* My Favorite Games*/
+console.log('My Favorite Games')
+
+console.log('1.) Mario Party 9 on the Nintendo Wii')
+console.log('I am not a huge gamer, so this is the only game I have fully completed multiple times.')
+
+console.log('2.) New Super Mario Bros Wii')
+console.log('I was never that good at it, but I have a lot of nostalgia playing this game, that came with the Wii, with my sister.')
+
+console.log('3.) Mario Kart')
+console.log('It is clear that the Wii is the only console I have ever owned, so this is another game that is full of nostalgia.')
+
+/*My Favorite Artists*/
+
+console.log('My Favorite Artists')
+
+console.log('1.) Keith Haring')
+console.log('Keith Haring is an artist culturally significant for having brought art to the streets, using vibrant, playful designs to champion social justice and spread messages of love, equality, and activism.')
+
+console.log('2.) Liza Lou')
+console.log('Liza Lou is an artist culturally significant for transforming everyday materials, like beads, into large-scale, labor-intensive artworks that challenge traditional boundaries of art and craft. She inspires artists like me to create large difficult work not only for the end result, but for the fun and experience of it.')
+
+console.log('3.) Frida Khalo')
+console.log('Frida Kahlo is an artist and symbol of resilience and self-expression, celebrated for her unique art that explores identity, pain, and her Mexican heritage. She inspires people like me worldwide to embrace indiviuality and defy societal norms.')
+
+/* Simple question and answer that uses prompt and the console.log.*/
+
+// Question
+let userAnswer = prompt("What is your favorite animal?");
+// Answer:
+console.log("The " + userAnswer + " is an amazing favorite animal!");
